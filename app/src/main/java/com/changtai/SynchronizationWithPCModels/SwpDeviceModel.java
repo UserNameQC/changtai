6f5a15b2e913e617fc7a698aa9d4caea3ba54014
286763739@qq.com <a123456>
2018-06-19 21:47:13 +0800

    修改gson相关的机井表实体类

--- app/src/main/java/com/changtai/SynchronizationWithPCModels/SwpDeviceModel.java
+++ app/src/main/java/com/changtai/SynchronizationWithPCModels/SwpDeviceModel.java
@@ -10,30 +10,43 @@ import java.util.Date;
  * 当数据上传时到PC时,把数据库实体通过程序映射为该实体,然后序列化为json数据,再上传到PC
  */
 public class SwpDeviceModel {
-    public String AreaNo;//新增加
+    //机井编号
     public String DeviceNo;
+    //售水站号
+    public String StationNo;
+    //GPRS模块编号
     public String GprsNo;
+    //机井名称,不维护,没用
     public String DeviceName;
+    //联系人
     public String Linkman;
+    //电话
     public String Phone;
+    //本地区机井索引（1=>999）
     public int Index;
+    //安装位置
     public String Location;
-    //售水断为Datetime类型
+    //登记时间
     public Date CreateDateTime;
-    //售水断为decimal类型
+    //经度
     public BigDecimal Longitude;
-    //售水断为decimal类型
+    //纬度
     public BigDecimal Latitude;
+    //备注
     public String Comment;
+    //操作员
     public String AdministratorName;
+    //停用标志
     public boolean StopFlag;
+    //服务器端数据版本
     public Long ServerVersion;//新增加
+    //电脑版售水软件数据版本
     public long ClientVersion;//新增加
+    //删除标志
     public boolean DelFlag;
 
 //    public Long Id;
 //    public Long BureauNo;
-//    public Long StationNo;
 //    public String TimeSpan;
 //    public long Version;
 }
